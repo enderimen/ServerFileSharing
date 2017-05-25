294dff86def1afb93861ceb148e94b591b3970c0
Ender IMEN <enderimen@hotmail.com>
2017-05-25 20:23:05 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ServerFileSharing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+      this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.groupBox1.SuspendLayout();
+      this.statusStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // textBox1
+      // 
+      resources.ApplyResources(this.textBox1, "textBox1");
+      this.textBox1.Name = "textBox1";
+      // 
+      // pictureBox1
+      // 
+      resources.ApplyResources(this.pictureBox1, "pictureBox1");
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.TabStop = false;
+      this.toolTip1.SetToolTip(this.pictureBox1, resources.GetString("pictureBox1.ToolTip"));
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.button2);
+      this.groupBox1.Controls.Add(this.button1);
+      this.groupBox1.Controls.Add(this.textBox1);
+      this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+      resources.ApplyResources(this.groupBox1, "groupBox1");
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.TabStop = false;
+      // 
+      // button2
+      // 
+      resources.ApplyResources(this.button2, "button2");
+      this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.button2.ForeColor = System.Drawing.SystemColors.Control;
+      this.button2.Name = "button2";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click_1);
+      // 
+      // button1
+      // 
+      resources.ApplyResources(this.button1, "button1");
+      this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Name = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click_1);
+      // 
+      // toolTip1
+      // 
+      this.toolTip1.AutomaticDelay = 50;
+      this.toolTip1.AutoPopDelay = 5000;
+      this.toolTip1.InitialDelay = 50;
+      this.toolTip1.ReshowDelay = 10;
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel3});
+      resources.ApplyResources(this.statusStrip1, "statusStrip1");
+      this.statusStrip1.Name = "statusStrip1";
+      // 
+      // toolStripStatusLabel1
+      // 
+      resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+      this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+      // 
+      // toolStripStatusLabel2
+      // 
+      resources.ApplyResources(this.toolStripStatusLabel2, "toolStripStatusLabel2");
+      this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+      // 
+      // toolStripStatusLabel4
+      // 
+      this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+      resources.ApplyResources(this.toolStripStatusLabel4, "toolStripStatusLabel4");
+      // 
+      // toolStripStatusLabel3
+      // 
+      resources.ApplyResources(this.toolStripStatusLabel3, "toolStripStatusLabel3");
+      this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+      // 
+      // timer1
+      // 
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // Form1
+      // 
+      this.AcceptButton = this.button1;
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.pictureBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.statusStrip1.ResumeLayout(false);
+      this.statusStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServerFileSharing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+          
+         /*
+         ##Author       :Ender İMEN 
+         ##Develop Time :Son Düzenleme Tarihi: 21.04.2017 | Saat: 23.38
+         ##NOT          :Çoklu dosya transfer işlemi için gereken kodlar yazılacak.
+         */
+        string sourcePath = @"C:\Program Files (x86)\Common Files\";//Dosyaların kopyalanacağı dizin
+        string kopyalanacakDosya = "", kopyalanacakDosyaIsmi = "", dosyanınKopyalacagiKlasor = "";
+        void kopyala()
+        {
+          openFileDialog1.Title = "Dosya Seçiniz...";
+
+          openFileDialog1.FileName = "";
+          this.openFileDialog1.Multiselect = true;
+          if (openFileDialog1.ShowDialog() == DialogResult.OK)
+          {
+            openFileDialog1.InitialDirectory = "C:\\";
+            kopyalanacakDosyaIsmi = openFileDialog1.SafeFileName.ToString();
+            kopyalanacakDosya = openFileDialog1.FileName.ToString();
+            textBox1.Text = kopyalanacakDosya;
+          }
+          else
+          {
+            MessageBox.Show("Dosya Seçmediniz...", "Uyarı..!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          } 
+        }//Kopylama Fonksiyonu
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            toolStripStatusLabel3.Text=DateTime.Now.ToLongDateString();
+        } //Gün 
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          toolStripStatusLabel2.Text = DateTime.Now.Hour.ToString() + ":"+DateTime.Now.Minute.ToString() + ":"+ DateTime.Now.Second.ToString();
+        }//Saat 
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+          DialogResult state = MessageBox.Show("Programı kapatmak istediğinizden emin misiniz?","Bilgi",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+          
+          if (state == DialogResult.No)
+          {
+            e.Cancel = true;
+          }
+          else { e.Cancel = false; }
+        }//Çıkış
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+          try
+          {
+          if (textBox1.Text!="")
+	        {
+            //Program Files (X86) yoksa Program Files ın içerisine kopyalar. 
+            /* if (File.Exists(sourcePath))
+             {
+               sourcePath = @"C:\Program Files\Common Files\";
+             }
+            */
+            dosyanınKopyalacagiKlasor = sourcePath;
+
+            if (dosyanınKopyalacagiKlasor != "" && kopyalanacakDosya != "")
+            {
+              if (File.Exists(dosyanınKopyalacagiKlasor + "" + kopyalanacakDosyaIsmi))
+              {
+                MessageBox.Show("Belirtilen klasörde " + kopyalanacakDosyaIsmi + " isimli dosya zaten mevcut...", "Uyarı..!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+              }
+              else
+              {
+                File.Copy(kopyalanacakDosya, dosyanınKopyalacagiKlasor + "" + kopyalanacakDosyaIsmi);
+                MessageBox.Show("Dosya Kopyalama İşlemi Başarılı", "Dosya Kopyalandı...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Clear();
+              }
+            }
+            else if (dosyanınKopyalacagiKlasor == "")
+            {
+              MessageBox.Show("Dosyanın Kopyalanacağı Klasörü Seçiniz...", "Uyarı..!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (kopyalanacakDosya == "")
+            {
+              MessageBox.Show("Dosya Seçiniz...", "Uyarı..!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+          }
+          else
+          {
+            MessageBox.Show("Dosya Seçiniz.", "Dikkat", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          }
+	        }
+          catch (Exception hata)
+          {
+            MessageBox.Show("Dosya Paylaşım Hatası!\n\n" + hata.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+          }   
+        }//Dosyanın hedef klasöre taşınması
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+          kopyala();
+        }//Kopylama
+    }
+}
+
+   
